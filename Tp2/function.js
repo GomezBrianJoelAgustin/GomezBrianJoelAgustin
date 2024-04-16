823d6511e28b5a8318f69f92b3c04f89a7530414
unknown <GomezBrian>
2024-04-16 10:27:10 -0300

    Tp2 casi terminado, solo tengo un error a la hora de mostrar los datos del primer personaje

--- Tp2/function.js
+++ Tp2/function.js
@@ -1,27 +1,46 @@
-function getCharacters(){
-
+function getCharacters() {
     const personajeId1 = document.getElementById("personajeId1").value;
     const personajeId2 = document.getElementById("personajeId2").value;
 
-    fetch(`https://rickandmortyapi.com/api/character/${personajeId1}, ${personajeId2}`)
+    fetch(`https://rickandmortyapi.com/api/character/${personajeId1},${personajeId2}`)
     .then(res => res.json())
-    .then(data =>{
+    .then(data => {
+        
+        console.log(data);
 
-        const infoPj1 = document.getElementById("infoPj1");
-        infoPj1.innerHTML =  ` 
+        const resultados = document.getElementById("resultados");
+    
+        data.forEach(pj =>{
+            const infoPjs = document.getElementById("infoPjs")
+ 
+            infoPjs.innerHTML =  `
+                        
+                <div class="info">  
+                    <img src="${pj.image}" alt="">
+                    <h3>Name: ${pj.name}</h3>
+                    <h3>Gender: ${pj.gender}</h3>
+                    <h3>Status: ${pj.status}</h3>
+                    <h3>Specie: ${pj.species}</h3>
+                </div>
+            `;
 
-        <div class="image-container">
-            <img src="${data.image}" alt="">
-        </div>
-        <div class="info">  
-            <h3>Name: ${data.name}</h3>
-            <h3>Gender: ${data.gender}</h3>
-        </div>
-        `
+            
+            if (data[0].gender == data[0+1].gender) {
+                match.innerHTML = `
+                <h1>hay match</h1>
+                `
+            } else {
+                match.innerHTML = `
+                <h1>no hay match</h1>
+                `
+            }
+        })
+
+        
+
+
     })
-
     .catch(error => {
         console.error(error);
-
     });
 }
