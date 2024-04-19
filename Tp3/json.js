9a9c1d0abff1b797404e297594f881da58a379c0
GomezBrianJoelAgustin <agustin.g2400@.com>
2024-04-19 20:49:00 -0300

    Falta hacer la parte del mapa del tp 3

--- Tp3/json.js
+++ Tp3/json.js
@@ -6,10 +6,13 @@ function userSearch(){
         .then (res => res.json())
         .then (data => {
 
-            console.log(data);
+            let latitudUser = data.results[0].location.coordinates.latitude;
+            let longitudeUser = data.results[0].location.coordinates.longitude;
 
-            let latitudUser = data.latitud;
-            let longitudeUser = data.longitude;
+            console.log(latitudUser)
+            console.log(longitudeUser)
+
+            let map = L.map("map").setView(latitudUser,longitudeUser);
      });
 
 }
