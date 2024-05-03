009c9587c7dd3b9b97d0307ec875801a3671495d
GomezBrianJoelAgustin <agustin.g2400@gmail.com>
2024-05-02 21:14:32 -0300

    Parcialito finalizado (solo falta css)

--- SimulacroDeParcial2/function.js
+++ SimulacroDeParcial2/function.js
@@ -4,10 +4,13 @@ const pjs2 = [];
 function buscarPersonajes1(){
 
     const id1 = document.getElementById("id1").value;
+    const btn1 = document.getElementById("btn1");
 
     pjs1.push(id1)
 
     if (pjs1.length == 3 ) {
+
+        btn1.Disable = true;
 
         alert("Ya se ingresaron 3 numeros")
 
@@ -20,11 +23,13 @@ function buscarPersonajes1(){
 function buscarPersonajes2(){
 
     const id2 = document.getElementById("id2").value;
+    const btn2 = document.getElementById("btn2");
 
     pjs2.push(id2)
 
     if (pjs2.length == 3 ) {
 
+        btn2.Disable = true;
         alert("Ya se ingresaron 3 numeros")
 
     };
