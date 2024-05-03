bf077cf8a02f7b85466a6dfe0f8f2b6a61617ce7
GomezBrianJoelAgustin <Agustin.g2400@gmail.com>
2024-05-03 19:08:06 -0300

    Agregue que no se puedan ingresar numeros que no esten en la api

--- SimulacroDeParcial2/Js/function.js
+++ SimulacroDeParcial2/Js/function.js
@@ -6,18 +6,24 @@ function buscarPersonajes1(){
     const id1 = document.getElementById("id1").value;
     const btn1 = document.getElementById("btn1");
 
-    pjs1.push(id1)
+    if (id1 >= 1 && id1 <=826) {
+            
+        pjs1.push(id1)
 
-    if (pjs1.length == 3 ) {
+        if (pjs1.length == 3 ) {
 
-        btn1.Disable = true;
+            btn1.Disable = true;
 
-        alert("Ya se ingresaron 3 numeros")
+            alert("Ya se ingresaron 3 numeros")
 
-    };
+        };
 
-    console.log("Los datos del primer array son: " + pjs1)
-    mostrarPjs(pjs1)
+        console.log("Los datos del primer array son: " + pjs1)
+        mostrarPjs(pjs1)
+
+    } else{
+        alert("Numero no valido")
+    }
 }
 
 function buscarPersonajes2(){
@@ -25,18 +31,23 @@ function buscarPersonajes2(){
     const id2 = document.getElementById("id2").value;
     const btn2 = document.getElementById("btn2");
 
-    pjs2.push(id2)
+    if (id2 >= 1 && id2 <=826) {
 
-    if (pjs2.length == 3 ) {
+        pjs2.push(id2)
 
-        btn2.Disable = true;
-        alert("Ya se ingresaron 3 numeros")
+        if (pjs2.length == 3 ) {
 
-    };
+            btn2.Disable = true;
+            alert("Ya se ingresaron 3 numeros")
 
-    console.log("Los datos del segundo array son: " + pjs2)
-    mostrarPjs2(pjs2)
+        };
 
+        console.log("Los datos del segundo array son: " + pjs2)
+        mostrarPjs2(pjs2)
+        
+    } else {
+        alert("Numero no valido")
+    }
 }
 
 function mostrarPjs(pjs1){
