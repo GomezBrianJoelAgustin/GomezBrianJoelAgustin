3d9612ff0bd6c2e36f33980bdaf8de2a94695e08
Gomez <agustin.g2400@gmail.com>
2024-05-09 21:07:45 -0300

    Agregue cosas de POO

--- POO/Function.js
+++ POO/Function.js
@@ -1,3 +1,7 @@
-let roma = new performance();
+class Rectangulo{
+    
+}
+
 //Todo lo de la izquierda se asigna lo que esta en la derecha
+//Los nombres de las clases en mayuscula la primera letra
 
